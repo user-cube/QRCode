2e2eed8442f5214cf46fcad08d471b32ffc0f8fd
Rui <ruicoelho@ua.pt>
2019-02-24 15:02:03 +0000

    Documentation

--- qrcode.py
+++ qrcode.py
@@ -1,10 +1,22 @@
 import pyqrcode
 from PIL import Image
 
+"""
+Function that generates the simple QRCode.
+
+Parameters
+-----
+linkToWebsite - Link to the Webiste that will be
+                stored at QRCode.
+"""
 def generateQR(linkToWebsite):
     url = pyqrcode.QRCode(linkToWebsite,error = 'H')
     url.png('qrcode.png',scale=10)
 
+"""
+Add logo to the center of a pre-generated QRCode.
+
+"""
 def addImage():
     im = Image.open('qrcode.png')
     im = im.convert("RGBA")
@@ -16,6 +28,10 @@ def addImage():
     im.paste(region,box)
     im.save("QRCodeLogo.png") 
 
+"""
+Menu function with options validation.
+
+"""
 def menu():
     print(" [2] - Generate code with logo\n [1] - Generate code without logo\n [0] - Exit")
     
@@ -39,4 +55,5 @@ def menu():
         generateQR(linkToWebsite)
         addImage()
 
+"""Main"""
 menu()
